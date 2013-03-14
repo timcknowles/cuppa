dda230b5ea885ba9d15c2caf5706516d732a3e16
Tim Knowles <tim.c.knowles@gmail.com>
2013-03-14 18:22:29 +0000

    set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuppa::Application.config.secret_token = '3ec4b7116bda6a08eb6bcc0444cd0243f3f3b812b7dfdf087039a8730a9581f56e8d0475e825b4ebc673dd49ea1ec6afc8284cb385a990d34c16084b34b9c52c'
