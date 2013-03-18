e244d4e5c3e5513581ded78358b0f3120dd12218
Tim Knowles <tim.c.knowles@gmail.com>
2013-03-18 21:44:37 +0000

    changing sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,7 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :courses
 
 
-  attr_accessor :initial_course_id
- 
-  after_create do
-  self.registrations.create(course_id: initial_course_id) if initial_course_id
+
 
 end
 
