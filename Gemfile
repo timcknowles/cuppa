a8d9d0606d1ce4e080c71b8b9120074dd1f497a9
Tim Knowles <tim.c.knowles@gmail.com>
2013-03-18 21:30:43 +0000

    restructure sign up

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.11'
 
 gem 'haml-rails'
 gem 'bootstrap-sass'
+gem 'bcrypt-ruby'
 
 
 # Gems used only for assets and not required
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ActiveRecord::Base
   has_many :courses, through: :registrations
   accepts_nested_attributes_for :courses
 
-  has_secure_password
-  validates_presence_of :password, :on => :create
 
   attr_accessor :initial_course_id
  
   after_create do
   self.registrations.create(course_id: initial_course_id) if initial_course_id
+
 end
+
 end
+
+
+  
