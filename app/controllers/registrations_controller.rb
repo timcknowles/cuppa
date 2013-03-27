a260f7ba1b2478aa0929852521992a5ff1ae80d7
Tim Knowles <tim.c.knowles@gmail.com>
2013-03-27 23:52:48 +0000

    landscape PDF

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,5 +10,6 @@ class RegistrationsController < ApplicationController
 
   def certificate
     @registration = current_user.registrations.find(params[:id])
+
   end
-end
+  end
--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,6 @@ module Cuppa
       g.template_engine :haml
     end
 
-    config.middleware.use "PDFKit::Middleware", :print_media_type => true
+    config.middleware.use "PDFKit::Middleware", :print_media_type => true, :orientation => 'Landscape'
   end
 end
