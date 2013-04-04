612575a9ec2dfd03ba9d78a79aacdb7c92b77373
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-04 18:02:29 +0100

    change to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,15 @@ Cuppa::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+
+  ActionController::Base.asset_host = Proc.new { |source, request|
+  if request.env["REQUEST_PATH"].include? ".pdf"
+    "file://#{Rails.root.join('public')}"
+  else
+    "#{request.protocol}#{request.host_with_port}"
+  end
+}
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
