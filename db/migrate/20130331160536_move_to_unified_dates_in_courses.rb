1d4677f44c7c16c0e8f1f7f2934da1cdb1610f80
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-09 13:27:45 +0100

    removal of migration to try and fix heroku error

--- db/migrate/20130331160536_move_to_unified_dates_in_courses.rb
+++
@@ -1,7 +0,0 @@
-class MoveToUnifiedDatesInCourses < ActiveRecord::Migration
-  def change
-    change_column :courses, :start_time, :datetime
-    change_column :courses, :end_time, :datetime
-    remove_column :courses, :date
-  end
-end
