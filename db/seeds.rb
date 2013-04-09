9dfc327a27c9887717e7345f922f8c6fcd0e04b4
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-09 11:56:28 +0100

    adding first user back to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+user = User.new 
+user.name = "Tim Knowles"
+user.mobile = ""
+user.email = "tim.c.knowles@gmail.com"
+user.password = "Foobar"
+
+user.admin = true
+user.save!
+
 
 require 'csv'
 
