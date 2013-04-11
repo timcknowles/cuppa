21612bebc3511e748de7ab74950f6e1bf28e528f
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-11 19:51:36 +0100

    trial of nested attributes close but not working mass asingment error

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,9 @@
 class Course < ActiveRecord::Base
-  attr_accessible :title, :description, :start_time, :end_time, :venue, :price, :places_available, :course_id
+  attr_accessible :title, :description, :start_time, :end_time, :venue, :price, :places_available, :course_id, :registrations_attributes
   has_many :registrations
   has_many :users, through: :registrations
   
+  accepts_nested_attributes_for :registrations, allow_destroy: false
 
   validates :title, presence: true
   validates :description, presence: true
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,13 +1,15 @@
 class Registration < ActiveRecord::Base
 
-  attr_accessible :course, :course_id, :feedback_form_completed
-  attr_accessible :feedback_form_completed, :as => :admin
+  attr_accessible :course, :course_id, :paid, :feedback_form_completed
+  #attr_accessible :feedback_form_completed, :as => :admin
 
   belongs_to :course
   belongs_to :user
   
   validate :course, presence: true
   validate :user, presence: true
+
+
   
   before_create do
     self.waiting_list = course.full?
