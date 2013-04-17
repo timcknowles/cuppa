4054a1f8888275a075d26c389e25f35214ee191f
Jeremy Walker <jeremy@meducation.net>
2013-04-17 23:19:38 +0100

    Scrap image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+=begin
 module ApplicationHelper
 
   def pdf_image_tag(filename, options = {})
@@ -5,5 +6,15 @@ module ApplicationHelper
   options[:src] = path.to_s
   attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
   raw("<img #{attributes}/>")
+  end
+end 
+=end
+
+module ApplicationHelper
+  def already_registered?(course)
+    if @already_registered === nil
+      @already_registered = current_user.registrations.where(course_id: course.id).exists?
+    end
+    @already_registered
   end
 end
