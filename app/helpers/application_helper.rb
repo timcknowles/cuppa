9f861de90bea2aca850e5087565a75ccbaf2821d
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-17 23:30:51 +0100

    app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
 
   def pdf_image_tag(filename, options = {})
-  path = Rails.root.join("app/assets/images/#{filename}")
-  options[:src] = path.to_s
-  attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
-  raw("<img #{attributes}/>")
+    path = Rails.root.join("app/assets/images/#{filename}")
+    options[:src] = path.to_s
+    attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
+    raw("<img #{attributes}/>")
   end
+  
+  def already_registered?
+    if already_registered === nil
+      @already_registered = current_user.registrations.where(course_id: params[:course_id]).exists?
+    end
+    @already_registered
+  end
 end
