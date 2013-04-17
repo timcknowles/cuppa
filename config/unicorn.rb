a33d6303285e7164d4a61232ec993f0f00f88e02
Jeremy Walker <jeremy@meducation.net>
2013-04-17 22:10:02 +0100

    Get unicorn definitely running

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,24 @@
-worker_processes 2
-preload_app true
-timeout 30
-listen 3000
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+ timeout 15
+ preload_app true
 
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
+ before_fork do |server, worker|
+
+   Signal.trap 'TERM' do
+     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+     Process.kill 'QUIT', Process.pid
+   end
+
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.connection.disconnect!
+ end 
+
+ after_fork do |server, worker|
+
+   Signal.trap 'TERM' do
+     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+   end
+
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.establish_connection
+ end
