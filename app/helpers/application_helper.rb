c0e2363ef1cbfae037b114030df4cf0ac7318949
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-17 21:51:06 +0100

    damn webkit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def pdf_image_tag(filename, options = {})
+  path = Rails.root.join("app/assets/images/#{filename}")
+  options[:src] = path.to_s
+  attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
+  raw("<img #{attributes}/>")
+  end
 end
--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -2,4 +2,12 @@ module RegistrationsHelper
   def register_btn_text(course)
     course.places_remaining > 0 ? "Register" : "Add me to the waiting list"
   end
+
+  def pdf_image_tag(filename, options = {})
+  path = Rails.root.join("app/assets/images/#{filename}")
+  options[:src] = path.to_s
+  attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
+  raw("<img #{attributes}/>")
+  end
 end
+
