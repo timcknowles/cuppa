d6e37d6338d52af2c1000f4a61cead8d861020bb
Tim Knowles <tim.c.knowles@gmail.com>
2013-04-28 21:28:32 +0100

    user mailer user.name and attachment img

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,18 +1,19 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "tim@harefieldstarcentre.org"
 
   def registration_confirmation(user)
-
+    @user = user
     mail(:to => user.email, :subject => "thanks for registering")
+    attachments["STaR_logo.gif"] = File.read("#{Rails.root}/app/assets/images/STaR_logo.gif")
   end
 
   def reminder(user)
-    
+    @user = user
     mail(:to => user.email, :subject => "reminder")
   end
 
   def feedback(user)
-    
+    @user = user
     mail(:to => user.email, :subject => "Please complete feedback")
   end
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -10,5 +10,5 @@ ActionMailer::Base.smtp_settings = {
 }
 
 require 'development_mail_interceptor'
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "cuppa.herokuapp.com"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
