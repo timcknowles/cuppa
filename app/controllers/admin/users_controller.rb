b97668fd78f510e22bbd185860756f7b87ea5c76
Tim Knowles <tim.c.knowles@gmail.com>
2013-07-27 17:59:53 +0100

    adding update action to admin users  controller to fix no update bug

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,6 +22,20 @@ class Admin::UsersController < AdminController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
     @course = User.find(params[:id])
     @course.destroy
