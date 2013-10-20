a422da55d038f7ee5364598f3e4eccf99bf38383
Jeremy Walker <jez.walker@gmail.com>
2013-10-21 00:39:44 +0100

    Add basic dashbaord code

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,9 +7,16 @@ class Admin::DashboardController < AdminController
   private
 
   def get_courses
+    if params[:start_time]
+      session[:admin_start_time] = Date.new(params[:start_time]["year"].to_i, params[:start_time]["month"].to_i)
+    end
+    if params[:end_time]
+      session[:admin_end_time] = Date.new(params[:end_time]["year"].to_i, params[:end_time]["month"].to_i)
+    end
     @start_time = session[:admin_start_time] || Date.new(2012,01,01)
     @end_time = session[:admin_end_time] || Date.today
-    @courses = Course.where("start_time >= ? and end_time <= ?", @start_date, @end_date)
-  end
 
+    @courses = Course.where("start_time >= ?", @start_time).
+      where("end_time <= ?", (Date.new(@end_time.year, @end_time.month, 1) + 1.month))
+  end
 end
