8db0109fb899caf40044a6ce3380bdf0369058b5
Tim Knowles <tim.c.knowles@gmail.com>
2013-11-13 12:08:27 +0000

    fixing actionmailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,20 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@harefieldstarcentre.org"
 
-  def registration_confirmation(user)
-    @user = user
+  def registration_confirmation(registration)
+    @user = registration.user
     @registration = registration
-    mail(:to => user.email, :subject => "Harefield STaR Centre: Thanks for registering")
+    mail(:to => registration.user.email, :subject => "Harefield STaR Centre: Thanks for registering")
     
   end
 
   def reminder(user)
     @user = user
-    mail(:to => user.email, :subject => "Harefield STaR Centre: Reminder")
+    mail(:to => registration.user.email, :subject => "Harefield STaR Centre: Reminder")
   end
 
   def feedback(user)
     @user = user
-    mail(:to => user.email, :subject => "Harefield STaR Centre: Please complete feedback")
+    mail(:to => registration.user.email, :subject => "Harefield STaR Centre: Please complete feedback")
   end
 end
